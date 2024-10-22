aeb7c39c1c3e25eac0f98bc769564e8930e19383
AndreasHansenAU <andreashh@live.dk>
2024-10-22 11:54:31 +0200

    Added infobox on click

--- src/map.py
+++ src/map.py
@@ -31,8 +31,7 @@ cache = Cache(app.server, config={
 # setup data
 @cache.memoize()
 def read_data_terror():
-    terror_encoding = 'ISO-8859-1'
-    df = pd.read_csv("src/data/globalterrorism_2020.csv", encoding=terror_encoding, low_memory=False)
+    df = pd.read_csv("src/data/globalterrorism_2020_cleaned.csv")
     df_jittered = add_jitter(df, "latitude", "longitude", "latitude_jitter", "longitude_jitter")
     return df_jittered
 
@@ -44,9 +43,17 @@ app.layout = html.Div([
     html.Div([
         dcc.Graph(
             id='map-heatmap',
-            hoverData={'points': [{'customdata': 'Denmark'}]}
+            hoverData=None,
+            clickData=None
         )], 
         style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}
+    ),
+
+    html.Div([
+        html.H4("Selected Attack Information", id="info-title"),
+        html.Div(id='info-box', style={'padding': '10px', 'border': '1px solid black', 'height': '200px'})
+    ],
+        style={'width': '49%', 'display': 'inline-block', 'vertical-align': 'top', 'padding': '0 20'}
     ),
 
     html.Div([
@@ -135,13 +142,25 @@ def update_map_heatmap(year_range, attacktype):
     )
 
     # update hover box
-    fig.update_traces(customdata=dff[['country_txt', 'iday', 'imonth', 'iyear', 'gname', 
-                                      'attacktype1_txt', 'weaptype1_txt', 'targtype1_txt']],
+    fig.update_traces(customdata=dff[['country_txt', 'iday', 'imonth', 'iyear',
+                                      'summary', 'crit1', 'crit2', 'crit3', 'related', 'region_txt',# 'provstate', 'city', 
+                                      'attacktype1_txt', #'attacktype2_txt', 'attacktype3_txt',
+                                      'success', 'suicide',
+                                      'weaptype1_txt', 'weapsubtype1_txt', #'weaptype2_txt', 'weapsubtype2_txt', 'weaptype3_txt', 'weapsubtype3_txt',
+                                      'targtype1_txt', 'targsubtype1_txt', #'targtype2_txt', 'targsubtype2_txt', 'targtype3_txt', 'targsubtype3_txt',
+                                      'corp1', #'corp2', 'corp3',
+                                      'target1', #'target2', 'target3',
+                                      'natlty1_txt', #'natlty2_txt', 'natlty3_txt',
+                                      'gname', 'guncertain1', 'nperps', 'motive',
+                                      'nkill', 'nkillter', 'nwound', 'nwoundte', 'property', 'propvalue', 'ishostkid', 'nhostkid', 'nhours', 'ndays',
+                                      'flag']],
+                      # update hover box
                       hovertemplate="<b>%{customdata[0]} %{customdata[1]}-%{customdata[2]}-%{customdata[3]}</b><br>"
-                                    "Group: %{customdata[4]}<br>"
-                                    "Primary attack type: %{customdata[5]}<br>"
-                                    "Primary weapon type used: %{customdata[6]}<br>"
-                                    "Primary target type: %{customdata[7]}<br>")
+                                    "Group: %{customdata[20]}<br>"
+                                    "Attack type: %{customdata[10]}<br>"
+                                    "Weapon type: %{customdata[13]}<br>"
+                                    "Target type: %{customdata[15]}<br>")
+
 
     # Update layout to add a title to the legend
     fig.update_layout(
@@ -152,6 +171,107 @@ def update_map_heatmap(year_range, attacktype):
     )
     
     return fig
+
+
+@callback(
+    Output('info-box', 'children'),
+    Input('map-heatmap', 'clickData'))
+def update_info_box(clickData):
+    if clickData is None:
+        return "Click on an attack to see details."
+
+    # Extract custom data from clickData
+    point_data = clickData['points'][0]['customdata']
+
+    # Overall details
+    country = point_data[0]
+    day = point_data[1]
+    month = point_data[2]
+    year = point_data[3]
+    summary = point_data[4]
+    crit1 = point_data[5]
+    crit2 = point_data[6]
+    crit3 = point_data[7]
+    related = point_data[8]
+    region_txt = point_data[9]
+    #provstate = point_data[10]
+    #city = point_data[11]
+    
+    # Attack types
+    attacktype1 = point_data[10]
+    #attacktype2 = point_data[13]
+    #attacktype3 = point_data[14]
+    
+    # Success and suicide
+    success = point_data[11]
+    suicide = point_data[12]
+    
+    # Weapon types and subtypes
+    weaptype1 = point_data[13]
+    weapsubtype1 = point_data[14]
+    #weaptype2 = point_data[19]
+    #weapsubtype2 = point_data[20]
+    #weaptype3 = point_data[21]
+    #weapsubtype3 = point_data[22]
+    
+    # Target types and subtypes
+    targtype1 = point_data[15]
+    targsubtype1 = point_data[16]
+    #targtype2 = point_data[25]
+    #targsubtype2 = point_data[26]
+    #targtype3 = point_data[27]
+    #targsubtype3 = point_data[28]
+    
+    # Corporate 
+    corp1 = point_data[17]
+    #corp2 = point_data[30]
+    #corp3 = point_data[31]
+    
+    # Target
+    target1 = point_data[18]
+    #target2 = point_data[33]
+    #target3 = point_data[34]
+    
+    # Target nationaly
+    natlty1 = point_data[19]
+    #natlty2 = point_data[36]
+    #natlty3 = point_data[37]
+    
+    # Groups
+    group = point_data[20]
+    guncertain = point_data[21]
+    nperps = point_data[22]
+    motive = point_data[23]
+    
+    # Casualties and injuries
+    nkill = point_data[24]
+    nkillter = point_data[25]
+    nwound = point_data[26]
+    nwoundte = point_data[27]
+    
+    # Property damage
+    property = point_data[28]
+    propvalue = point_data[29]
+    
+    # Hostage information
+    ishostkid = point_data[30]
+    nhostkid = point_data[31]
+    nhours = point_data[32]
+    ndays = point_data[33]
+    
+    # Flag
+    flag = point_data[34]
+
+    # Return a formatted string to display in the info box
+    return html.Div([
+        html.P(f"Country: {country} {flag}"),
+        html.P(f"Date: {day}-{month}-{year}"),
+        html.P(f"Group: {group}"),
+        html.P(f"Attack Type: {attacktype1}"),
+        html.P(f"Weapon Type: {weaptype1}"),
+        html.P(f"Target Type: {targtype1}"),
+        html.P(summary)
+    ])
 
 
 @callback(
